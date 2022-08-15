b42dd0f41e1731daa30241a3be233225557f21ff
pawan-nirpal-031 <pawannirpal@gmail.com>
2022-08-15 21:12:41 +0530

    Rook And Bishop inflight blocker mask computation

--- DirtyAttempt/BitBoardChess.cpp
+++ DirtyAttempt/BitBoardChess.cpp
@@ -85,25 +85,78 @@ public:
         return attacks;
     }
 
-    ull InFlightBishopAttackMask(int square,SupportAndUtils &supp_bot){
+    ull InFlightBishopAttackMask(int square,ull blocker,SupportAndUtils &supp_bot){
         ull attacks = 0ULL; 
         int rank = square / 8,file = square % 8; 
-        for(int tar_rank = rank+1,tar_file = file+1; (tar_rank<7 and tar_file <7); tar_file++, tar_rank++)  attacks|=(1ULL<<supp_bot.GetSqaure(tar_rank,tar_file));
-        for(int tar_rank = rank-1,tar_file = file+1; (tar_rank>=1 and tar_file <7); tar_file++, tar_rank--)  attacks|=(1ULL<<supp_bot.GetSqaure(tar_rank,tar_file));
-        for(int tar_rank = rank-1,tar_file = file-1; (tar_rank>=1 and tar_file>=1); tar_file--, tar_rank--)  attacks|=(1ULL<<supp_bot.GetSqaure(tar_rank,tar_file));
-        for(int tar_rank = rank+1,tar_file = file-1; (tar_rank<7 and tar_file >=1); tar_file--, tar_rank++)  attacks|=(1ULL<<supp_bot.GetSqaure(tar_rank,tar_file));
+        for(int tar_rank = rank+1,tar_file = file+1; (tar_rank<=7 and tar_file <=7); tar_file++, tar_rank++){
+            int tar_sq = supp_bot.GetSqaure(tar_rank,tar_file);
+            attacks|=(1ULL<<tar_sq);
+            if((1ULL<<tar_sq) & blocker) break;
+        }
+        for(int tar_rank = rank-1,tar_file = file+1; (tar_rank>=0 and tar_file <=7); tar_file++, tar_rank--){
+            int tar_sq = supp_bot.GetSqaure(tar_rank,tar_file);
+            attacks|=(1ULL<<tar_sq);
+            if((1ULL<<tar_sq) & blocker) break;
+        }
+        for(int tar_rank = rank-1,tar_file = file-1; (tar_rank>=0 and tar_file>=0); tar_file--, tar_rank--){
+            int tar_sq = supp_bot.GetSqaure(tar_rank,tar_file);
+            attacks|=(1ULL<<tar_sq);
+            if((1ULL<<tar_sq) & blocker) break;
+        }
+        for(int tar_rank = rank+1,tar_file = file-1; (tar_rank<=7 and tar_file >=0); tar_file--, tar_rank++){
+            int tar_sq = supp_bot.GetSqaure(tar_rank,tar_file);
+            attacks|=(1ULL<<tar_sq);
+            if((1ULL<<tar_sq) & blocker) break;
+        }
         return attacks;
-    } 
+    }  
+
+    ull InFlightRookAttackMask(int square,ull blocker,SupportAndUtils &supp_bot){
+        ull attacks = 0ULL; 
+        int rank = square / 8,file = square % 8; 
+        for(int tar_file = file+1; (tar_file <=7); tar_file++){
+            int tar_sq = supp_bot.GetSqaure(rank,tar_file);
+            attacks|=(1ULL<<tar_sq);
+            if((1ULL<<tar_sq) & blocker) break;
+        }
+        for(int tar_file = file-1; (tar_file >=0); tar_file--){
+            int tar_sq = supp_bot.GetSqaure(rank,tar_file);
+            attacks|=(1ULL<<tar_sq);
+            if((1ULL<<tar_sq) & blocker) break;
+        }
+        for(int tar_rank = rank+1; (tar_rank <=7); tar_rank++){
+            int tar_sq = supp_bot.GetSqaure(tar_rank,file);
+            attacks|=(1ULL<<tar_sq);
+            if((1ULL<<tar_sq) & blocker) break;
+        }
+        for(int tar_rank = rank-1; (tar_rank >=0); tar_rank--) {
+            int tar_sq = supp_bot.GetSqaure(tar_rank,file);
+            attacks|=(1ULL<<tar_sq);
+            if((1ULL<<tar_sq) & blocker) break;
+        }
+        return attacks;
+    }
 };
 
 
 int main(){
     SupportAndUtils support; 
-    AttackSystem attksys(support); 
+    AttackSystem attksys(support);  
+    ull block = 0ull;  
+    support.SetBit(block,d7); 
+    support.SetBit(block,d1);
+    support.SetBit(block,d2);
+    support.SetBit(block,b4); 
+    support.SetBit(block,g4);  
+    support.PrintBitBoard(block);
+
+    ull inflight =  attksys.InFlightRookAttackMask(d4,block,support); 
+    support.PrintBitBoard(inflight);
+
     for(int sq =0;sq<64;sq++){
        // support.PrintBitBoard(attksys.king_attack_table[sq]); 
-       ull board =  attksys.RookAttackMask(sq,support);
-       support.PrintBitBoard(board);
+      // ull board =  attksys.InFlightBishopAttackMask(sq,support);
+     //  support.PrintBitBoard(board);
     }
     return 0;
 }
