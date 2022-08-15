18037dfe613d8ce174b1d94049878e86e7f022d7
pawan-nirpal-031 <pawannirpal@gmail.com>
2022-08-15 15:29:42 +0530

    King attack table done

--- DirtyAttempt/BitBoardChess.cpp
+++ DirtyAttempt/BitBoardChess.cpp
@@ -1,9 +1,24 @@
 #include "Support.h"
 
 class AttackSystem{
+
+    void InitLeaperAttacks(SupportAndUtils &supp_bot){
+        for(int sq =0;sq<64;sq++){
+            pawn_attack_table[white][sq] = this->PawanAttackMask(white,sq,supp_bot);
+            pawn_attack_table[black][sq] = this->PawanAttackMask(black,sq,supp_bot); 
+            knight_attack_table[sq] = this->KnightAttackMask(sq,supp_bot);
+            king_attack_table[sq] = this->KingAttackMask(sq,supp_bot);
+        }
+    }  
+
 public:
     U64 pawn_attack_table[2][64];  
-    U64 knight_attack_table[64];                 
+    U64 knight_attack_table[64];      
+    U64 king_attack_table[64];           
+
+    AttackSystem(SupportAndUtils &supp_bot){
+        InitLeaperAttacks(supp_bot);
+    }
 
     U64 PawanAttackMask(int side,int square, SupportAndUtils &supp_bot){      
         U64 attacks = 0ULL;                         
@@ -17,18 +32,6 @@ public:
             if(not_a_file & (pawn_bit_board << 9)) attacks |=(pawn_bit_board << 9);
         } 
         return attacks;                             
-    }  
-
-    U64 GetPawnBitBoard(int side,int sqaure){
-        return pawn_attack_table[side][sqaure];
-    }
-
-    void InitLeaperAttacks(SupportAndUtils &supp_bot){
-        for(int sq =0;sq<64;sq++){
-            pawn_attack_table[white][sq] = this->PawanAttackMask(white,sq,supp_bot);
-            pawn_attack_table[black][sq] = this->PawanAttackMask(black,sq,supp_bot); 
-            knight_attack_table[sq] = this->KnightAttackMask(sq,supp_bot);
-        }
     }  
 
     U64 KnightAttackMask(int square, SupportAndUtils &supp_bot){
@@ -46,16 +49,33 @@ public:
         if(not_hg_file & (pieceBitBoard<<6)) attacks|=(pieceBitBoard<<6); 
         return attacks;
     } 
+
+    U64 KingAttackMask(int square,SupportAndUtils &supp_bot){
+        U64 attacks = 0ULL; 
+        U64 pieceBitBoard = 0ULL; 
+        supp_bot.SetBit(pieceBitBoard,square);
+        if((pieceBitBoard>>1) & not_h_file) attacks|=(pieceBitBoard>>1);
+        if((pieceBitBoard>>7) & (not_a_file)) attacks|=(pieceBitBoard>>7); 
+        if((pieceBitBoard>>8)) attacks|=(pieceBitBoard>>8);
+        if((pieceBitBoard>>9) & (not_h_file)) attacks|=(pieceBitBoard>>9); 
+        if((pieceBitBoard<<1) & not_a_file) attacks|=(pieceBitBoard<<1);
+        if((pieceBitBoard<<7) & (not_h_file)) attacks|=(pieceBitBoard<<7); 
+        if((pieceBitBoard<<8)) attacks|=(pieceBitBoard<<8);
+        if((pieceBitBoard<<9) & (not_a_file)) attacks|=(pieceBitBoard<<9);
+        return attacks;
+    }
+
 };
 
 
 int main(){
     U64 board = 0ULL;  
     SupportAndUtils support; 
-    AttackSystem attksys; 
-    attksys.InitLeaperAttacks(support);
+    AttackSystem attksys(support);  
+    // U64 kam =  attksys.KingAttackMask(h1,support);
+    // support.PrintBitBoard(kam);
     for(int sq =0;sq<64;sq++){
-        support.PrintBitBoard(attksys.knight_attack_table[sq]);
+        support.PrintBitBoard(attksys.king_attack_table[sq]);
     }
     return 0;
 }
